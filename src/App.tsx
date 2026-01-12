a35415def564a67138ed40ca1821f5b525c4ae2b
Guilherme Branco Rodrigues <guidriguesbrnc@gmail.com>
2026-01-12 01:07:39 -0300

    Primeiro commit - projeto já iniciado previamente.

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu'
+
+import './styles/theme.css';
+import './styles/global.css';
+
+function App() {
+    return (
+        <>
+            <Container>
+                <Logo />
+            </Container>
+            
+            <Container>
+                <Menu />
+            </Container>
+        </>
+    );
+};
+
+export { App };
